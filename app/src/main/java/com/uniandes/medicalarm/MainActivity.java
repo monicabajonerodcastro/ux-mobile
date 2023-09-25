00d00b95370c1aa6e2cab6c9d5e87f66472e89f3
monicabajonerodcastro <m.bajonero@uniandes.edu.co>
2023-09-24 21:24:04 -0500

    Pantalla de registro

--- app/src/main/java/com/uniandes/medicalarm/MainActivity.java
+++ app/src/main/java/com/uniandes/medicalarm/MainActivity.java
@@ -2,13 +2,29 @@ package com.uniandes.medicalarm;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button registarse_button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        registarse_button = findViewById(R.id.registrar);
+        registarse_button.setOnClickListener(
+                new View.OnClickListener(){
+                    @Override
+                    public void onClick(View view){
+                        Intent intent = new Intent(MainActivity.this, RegistroActivity.class);
+                        startActivity(intent);
+                    }
+                }
+        );
     }
 }
